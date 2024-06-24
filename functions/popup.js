0e7d8d9fbc60a356c2dc06a606982128e3dcd44d
emilezi <75627932+emilezi@users.noreply.github.com>
2024-06-24 15:24:07 +0200

    Add files via upload

--- functions/popup.js
+++ functions/popup.js
@@ -1,8 +1,18 @@
+const language = (navigator.language || navigator.userLanguage).substr(0, 2);
+
 function PopUpRadio() {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";   
+
+    }else{
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";
+        
+    }
 
 }
 
@@ -10,7 +20,15 @@ function PopUpNew(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_" + value + "' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";   
+        
+    }
 
 }
 
@@ -18,7 +36,15 @@ function PopUpApprove(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Approbation de l'idée : </h5><form method='post'><input name='post_comment' type='text' placeholder='Saisissez un commentaire' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_approuve_" + value + "' value='Approuver l&rsquo;idée'/><input type='submit' onclick=PopUpClose() value='Annuler'></div></form></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Approbation de l'idée :</h5><form method='post'><input name='post_comment' type='text' placeholder='Saisissez un commentaire' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_approuve_" + value + "' value='Approuver l&rsquo;idée'/><input type='submit' onclick=PopUpClose() value='Annuler'></div></form></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Approval of the idea :</h5><form method='post'><input name='post_comment' type='text' placeholder='Enter a comment' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_approuve_" + value + "' value='Approve the idea'/><input type='submit' onclick=PopUpClose() value='Cancel'></div></form></div></div>";   
+        
+    }
 
 }
 
@@ -26,7 +52,15 @@ function PopUpRefuse(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Suppression de l'idée : </h5><form method='post'><input name='post_comment' type='text' placeholder='Saisissez un commentaire' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_refuse_" + value + "' value='Supprimer l&rsquo;idée'/><input type='submit' onclick=PopUpClose() value='Annuler'></div></form></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Suppression de l'idée :</h5><form method='post'><input name='post_comment' type='text' placeholder='Saisissez un commentaire' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_refuse_" + value + "' value='Supprimer l&rsquo;idée'/><input type='submit' onclick=PopUpClose() value='Annuler'></div></form></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Removing the idea :</h5><form method='post'><input name='post_comment' type='text' placeholder='Enter a comment' required/><br/><br/><div class='btn-content-popup'><input type='submit' name='submit_refuse_" + value + "' value='Delete idea'/><input type='submit' onclick=PopUpClose() value='Cancel'></div></form></div></div>";   
+        
+    }
 
 }
 
@@ -34,7 +68,15 @@ function PopUpDelete(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_delete_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_delete_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_delete_" + value + "' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";   
+        
+    }
 
 }
 
@@ -42,7 +84,15 @@ function PopUpVoteFor(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_for_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_for_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_for_" + value + "' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";
+        
+    }
 
 }
 
@@ -50,7 +100,15 @@ function PopUpVoteAgainst(value) {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_against_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_against_" + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='submit_popup_against_" + value + "' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";
+        
+    }
 
 }
 
@@ -58,7 +116,15 @@ function PopUpPictureUpload() {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Importer une image</h5><form enctype='multipart/form-data' method='post'><input type='file' name='picture'/><input type='submit' name='submit_picture' value='Importer'/><br><br><input type='submit' onclick=PopUpClose() value='Annuler'></form></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Importer une image</h5><form enctype='multipart/form-data' method='post'><input type='file' name='picture'/><input type='submit' name='submit_picture' value='Importer'/><br><br><input type='submit' onclick=PopUpClose() value='Annuler'></form></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Import an image</h5><form enctype='multipart/form-data' method='post'><input type='file' name='picture'/><input type='submit' name='submit_picture' value='Import'/><br><br><input type='submit' onclick=PopUpClose() value='Cancel'></form></div></div>";
+        
+    }
 
 }
 
@@ -66,7 +132,15 @@ function PopUpVideoUpload() {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Importer une vidéo</h5><form enctype='multipart/form-data' method='post'><input type='file' name='video'/><input type='submit' name='submit_video' value='Importer'/><br><br><input type='submit' onclick=PopUpClose() value='Annuler'></form></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Importer une vidéo</h5><form enctype='multipart/form-data' method='post'><input type='file' name='video'/><input type='submit' name='submit_video' value='Importer'/><br><br><input type='submit' onclick=PopUpClose() value='Annuler'></form></div></div>";
+
+    }else{
+        
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Import a video</h5><form enctype='multipart/form-data' method='post'><input type='file' name='video'/><input type='submit' name='submit_video' value='Import'/><br><br><input type='submit' onclick=PopUpClose() value='Cancel'></form></div></div>";
+        
+    }
 
 }
 
